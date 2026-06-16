79021474f0b38bf0a95e2ba6c6df7d950a2bdbe6
Andrey <andrewenka@gmail.com>
2026-06-16 22:41:03 +0300

    Fix deprecations.

--- src/Repository/ProductRepository.php
+++ src/Repository/ProductRepository.php
@@ -9,48 +9,35 @@ use Doctrine\ORM\EntityRepository;
 class ProductRepository extends EntityRepository
 {
     public function findAllOrderedByName() {
-        return $this->getEntityManager()
-            ->createQuery(
-                'SELECT p FROM App:Product p ORDER BY p.name ASC',
-            )
-            ->getResult();
+        $qb = $this->createQueryBuilder('p')
+            ->orderBy('p.name', 'ASC');
+
+        return $qb->getQuery()->getResult();
     }
 
     public function findBySubcategoryManufacturer(int $subcategoryId, int $manufacturerId) {
-        $em = $this->getEntityManager();
-        $qb = $em->createQueryBuilder();
-        $qb->select('p')
-            ->from('App:Product', 'p')
+        $qb = $this->createQueryBuilder('p')
             ->innerjoin('p.productManufacturers', 'pm')
             ->where('p.subcategory = :subcategoryId AND pm.manufacturer = :manufacturerId')
             ->orderBy('p.seq', 'ASC')
             ->setParameter('subcategoryId', $subcategoryId)
             ->setParameter('manufacturerId', $manufacturerId);
 
-        $products = $qb->getQuery()->getResult();
-
-        return $products;
+        return $qb->getQuery()->getResult();
     }
 
     public function findBySubcategory(int $subcategoryId) {
-        $em = $this->getEntityManager();
-        $qb = $em->createQueryBuilder();
-        $qb->select('p')
-            ->from('App:Product', 'p')
+        $qb = $this->createQueryBuilder('p')
             ->where('p.subcategory = :subcategoryId')
             ->orderBy('p.seq', 'ASC')
             ->setParameter('subcategoryId', $subcategoryId);
 
-        $products = $qb->getQuery()->getResult();
-
-        return $products;
+        return $qb->getQuery()->getResult();
     }
 
     public function getSeqForNewProductInSubcategory(int $subcategoryId): int {
-        $em = $this->getEntityManager();
-        $qb = $em->createQueryBuilder();
-        $qb->select('max(p.seq) as maxseq')
-            ->from('App:Product', 'p')
+        $qb = $this->createQueryBuilder('p')
+            ->select('max(p.seq) as maxseq')
             ->where('p.subcategory = :subcategoryId')
             ->setParameter('subcategoryId', $subcategoryId);
 
