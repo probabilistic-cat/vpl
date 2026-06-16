722593089eb4a705a8c167c5d2898c3886f75d06
Andrey <andrewenka@gmail.com>
2026-06-16 22:50:15 +0300

    Update PHP to 8.5.

--- rector.php
+++ rector.php
@@ -22,7 +22,7 @@ return RectorConfig::configure()
     ])
     ->withSets([
         DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
-        LevelSetList::UP_TO_PHP_84,
+        LevelSetList::UP_TO_PHP_85,
         SetList::CODE_QUALITY,
         SetList::DEAD_CODE,
         SetList::EARLY_RETURN,
