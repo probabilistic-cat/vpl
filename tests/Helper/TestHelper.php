ef3690247b73fdd1fb0c21610ecf0fb6f41465f7
Andrey <andrewenka@gmail.com>
2026-06-27 08:57:31 +0300

    Cleanup after all phpunit tests.

--- tests/Helper/TestHelper.php
+++ tests/Helper/TestHelper.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace App\Tests\Helper;
 
+use App\Helper\FileHelper;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class TestHelper
 {
-    private const string FIXTURES_IMG_DIR = __DIR__ . '/../Fixtures/img/';
-    private const string IMG_NAME_PREFIX = 'img_test_';
+    private const string FIXTURES_IMG_DIR = FileHelper::DIR_PROJECT . 'tests/Fixtures/img/';
+    private const string FIXTURE_IMG_NAME_PREFIX = 'img_test_';
 
     public static function getImgFile(): UploadedFile {
         $index = mt_rand(0, 9);
@@ -29,7 +30,22 @@ class TestHelper
         return $maxLength < 32 ? substr($string, 0, $maxLength) : $string;
     }
 
+    public static function cleanupFixtures(): void {
+        $fixtureImgList = array_diff(scandir(self::FIXTURES_IMG_DIR), ['.', '..']);
+        $deletedFiles = [];
+        foreach ($fixtureImgList as $fileName) {
+            if (preg_match('~^' . self::FIXTURE_IMG_NAME_PREFIX . '\d\.jpg$~', $fileName) !== 1) {
+                @unlink(self::FIXTURES_IMG_DIR . $fileName);
+                $deletedFiles[] = $fileName;
+            }
+        }
+
+        if (count($deletedFiles) > 0) {
+            echo "\n\nDeleted remaining files:\n- " . implode("\n- ", $deletedFiles);
+        }
+    }
+
     private static function getFileName(string $name): string {
-        return self::IMG_NAME_PREFIX . $name . '.jpg';
+        return self::FIXTURE_IMG_NAME_PREFIX . $name . '.jpg';
     }
 }
